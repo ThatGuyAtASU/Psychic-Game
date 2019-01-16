d8b07e453fb9ecb108f15f89e7ec2f16c8aa28e8
Sum Guy <gjserq@gmail.com>
2019-01-16 16:33:07 -0700

    Wednesday Push

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,8 +12,17 @@ var wins = 0;
 var losses = 0;
 var guesses = 9;
 
+
+var $wins = document.getElementById("winTotal");
+var $losses = document.getElementById("lossesTotal");
+var $guesses = document.getElementById("guesses");
 var alphabet = generateAlphabet();
 var $alreadyGuessed = document.getElementById("already-guessed");
+
+document.querySelector("#guesses").innerHTML = 9;
+document.querySelector("#winTotal").innerHTML = 0;
+document.querySelector("#lossesTotal").innerHTML = 0;
+
 console.log($alreadyGuessed);
 
 var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
@@ -23,37 +32,48 @@ console.log("Computer Guess:", computerGuess);
 var alreadyGuessedArr = [];
 
 document.onkeyup = function (event) {
-  var userGuess = event.key;
-  alreadyGuessedArr.push(userGuess);
+  if (alreadyGuessedArr.length <= 9) {
+    var userGuess = event.key.toLowerCase();
+
+    alreadyGuessedArr.push(userGuess);
 
 
-  $alreadyGuessed.textContent = alreadyGuessedArr.join(",   ");
+    $alreadyGuessed.textContent = alreadyGuessedArr.join(",   ");
 
-  if (userGuess !== computerGuess) {
-    guesses--;
-    console.log("Guesses:", guesses);
+
+    //if else statement to set value of wins, losses, and computerGuess
+    if (userGuess !== computerGuess) {
+      guesses--;
+      document.querySelector("#guesses").innerHTML = guesses;
+      console.log("Guesses:", guesses);
+    }
+    else {
+      wins++;
+      document.querySelector("#winTotal").innerHTML = wins;
+      console.log("Wins:", wins);
+      guesses = 8;
+      computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
+      alreadyGuessedArr = [];
+      console.log("Computer Guess:", computerGuess)
+    }
+
+    if (guesses === 0) {
+      losses++;
+      document.querySelector("#lossesTotal").innerHTML = losses;
+      console.log("Losses: ", losses);
+      guesses = 8;
+      alreadyGuessedArr=[];
+      computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
+      console.log("Computer Guess:", computerGuess)
+
+    } 
   }
+ 
+
   else {
-    wins++;
-    console.log("Wins:", wins);
-    guesses = 9;
-    computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
-    console.log("Computer Guess:", computerGuess)
+    alreadyGuessedArr = [];
+
   }
 
-  if (guesses === 0) {
-    losses++;
-    console.log("Losses: ", losses);
-    guesses = 9;
-    computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
-    console.log("Computer Guess:", computerGuess)
-  }
 
 }
-
-
-//for loop where when i hits 8 losses goes up by one?
-//if statement with if(userGuess!==computerGuess) responds with losses++
-// Guesses Left--
-//else wins ++
-//
